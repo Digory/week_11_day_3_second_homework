a405c28a9d3e9b9a098dbc187d10aae29367e75c
Digory <digory1@googlemail.com>
2018-09-05 18:42:38 +0100

    pangram_finder tests passed

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,19 @@
 const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.alphabetArray = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phraseArray = phrase.toLowerCase().split(' ');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  const phraseLettersArray = [];
+  this.phraseArray.forEach(word=>{
+    const letterArray = word.split('');
+    for(const letter of letterArray){
+      phraseLettersArray.push(letter);
+    }
+  });
+  return this.alphabetArray.every(letter=>{
+    return phraseLettersArray.includes(letter);
+  });
 }
 
 module.exports = PangramFinder;
