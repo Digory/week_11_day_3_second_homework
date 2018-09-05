b4cf0f00fcd0eda642396a9695072838e78ddc11
Digory <digory1@googlemail.com>
2018-09-05 18:52:37 +0100

    isogram_finder tests passed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,17 @@
 const IsogramFinder = function (word) {
-
+    this.wordAsArray = word.toLowerCase().split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    const lettersObject = {};
+    for(const letter of this.wordAsArray){
+        if(lettersObject[letter]){
+            return false;
+        }else{
+            lettersObject[letter] = 1;
+        }
+    }
+    return true;
 }
 
 module.exports = IsogramFinder;
--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,19 +1,23 @@
 const PangramFinder = function (phrase) {
   this.alphabetArray = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-  this.phraseArray = phrase.toLowerCase().split(' ');
+  this.phraseAsWordArray = phrase.toLowerCase().split(' ');
 }
 
 PangramFinder.prototype.isPangram = function () {
+  return this.alphabetArray.every(letter=>{
+    return this.getPhraseAsLettersArray().includes(letter);
+  });
+}
+
+PangramFinder.prototype.getPhraseAsLettersArray = function () {
   const phraseLettersArray = [];
-  this.phraseArray.forEach(word=>{
+  this.phraseAsWordArray.forEach(word=>{
     const letterArray = word.split('');
     for(const letter of letterArray){
       phraseLettersArray.push(letter);
     }
   });
-  return this.alphabetArray.every(letter=>{
-    return phraseLettersArray.includes(letter);
-  });
+  return phraseLettersArray;
 }
 
 module.exports = PangramFinder;
